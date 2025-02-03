a5ff0dc019195e6c78d746526ae45daf5001e256
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-02-03 13:00:40 +0100

    update ui plugins

--- .config/nvim/lua/plugins/ui/init.lua
+++ .config/nvim/lua/plugins/ui/init.lua
@@ -1,9 +1,8 @@
 return {
-  require("plugins.ui.colorscheme"),
-  require("plugins.ui.alpha"),
-  require("plugins.ui.bufferline"),
-  require("plugins.ui.lualine"),
-  require("plugins.ui.neo-tree"),
-  require("plugins.ui.smear"),
-  require("plugins.ui.which-key"),
+	require("plugins.ui.colorscheme"),
+	require("plugins.ui.alpha"),
+	require("plugins.ui.bufferline"),
+	require("plugins.ui.lualine"),
+	require("plugins.ui.neo-tree"),
+	require("plugins.ui.which-key"),
 }
