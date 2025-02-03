236705cec1b4d1262f2ce5aee7d55619d59cf744
Thrasos <100732009+thrasosc@users.noreply.github.com>
2025-02-03 13:00:28 +0100

    update nvim plugins

--- .config/nvim/lua/plugins/editor/init.lua
+++ .config/nvim/lua/plugins/editor/init.lua
@@ -1,7 +1,6 @@
 return {
-  require("plugins.editor.autopairs"),
-  require("plugins.editor.flash"),
-  require("plugins.editor.neoscroll"),
-  require("plugins.editor.surround"),
-  require("plugins.editor.treesitter"),
+	require("plugins.editor.autopairs"),
+	require("plugins.editor.flash"),
+	require("plugins.editor.surround"),
+	require("plugins.editor.treesitter"),
 }
