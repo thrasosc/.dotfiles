8437477ddcd675786f000b45280c9941501d93d5
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-05-30 00:22:07 +0200

    add neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,32 @@
+require("config.lazy")
+
+-- General Settings
+vim.cmd("setlocal spell spelllang=en_gb")
+vim.cmd("set mouse=a")
+vim.cmd("set clipboard=unnamed") -- on Linux this needs to be unnamedplus
+
+-- Interface
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set nowrap")
+vim.cmd("set showcmd")
+vim.cmd("set showmode")
+vim.cmd("set showmatch")
+vim.cmd("set termguicolors")
+vim.cmd("set wildmode=longest,list,full")
+vim.cmd("set background=dark")
+vim.cmd([[colorscheme gruvbox]])
+
+-- Indentation
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set expandtab")
+vim.cmd("set autoindent")
+
+-- Searching
+vim.cmd("set hlsearch")
+vim.cmd("set incsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+
