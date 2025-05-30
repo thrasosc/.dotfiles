a08a31a65e570b490b95bdb6fa92979c2d4a9a2c
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-05-30 04:02:26 +0200

    add neovim formatting plugins

---
+++ .config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim", -- Ensure installed formatters, linters, etc. (mason-lspconfig only downloads LSPs)
+		opts = {
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"black",
+				"isort",
+				"clang-format",
+			},
+		},
+	},
+	{
+		"nvimtools/none-ls.nvim", -- Integrates formatters and linters by simulating an LSP server
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.formatting.clang_format,
+				},
+			})
+		end,
+	},
+}
