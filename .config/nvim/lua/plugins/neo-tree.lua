94c7cac198563e91e4f66cd9a0879f82ef9302bc
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-05-30 05:24:04 +0200

    update neo-tree config

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,19 +1,20 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    ---@module "neo-tree"
-    ---@type neotree.Config?
-    opts = {
-        -- fill any relevant options here
-    },
-    config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle right<CR>', { desc = 'Toggle Neotree' })
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	lazy = false, -- neo-tree will lazily load itself
+	---@module "neo-tree"
+	---@type neotree.Config?
+	opts = {
+		source_selector = {
+			winbar = true,
+		},
+	},
+	keys = {
+		{ "<C-n>", ":Neotree toggle right<CR>", desc = "Toggle Neotree" },
+	},
 }
