a32d5435b6b2022a889253329dc3a910833bce6d
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-05-30 04:03:57 +0200

    auto install treesitter configs instead of installing all of them by default

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require'nvim-treesitter.configs'.setup {
-            ensure_installed = "all",
+            auto_install = true,
             highlight = { enable = true },
             indent = { enable = true }
         }
