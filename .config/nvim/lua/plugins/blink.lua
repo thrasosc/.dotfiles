10d65d958ee3aad036758c11a7179c4e1ad4cb5c
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-06-05 18:10:05 +0200

    cleanup blink config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,19 +1,13 @@
 return {
-	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
-	dependencies = { "rafamadriz/friendly-snippets" },
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = { "rafamadriz/friendly-snippets" },
 
-	-- use a release tag to download pre-built binaries
-	version = "1.*",
+    -- use a release tag to download pre-built binaries
+    version = "1.*",
 
-	opts = {
-		keymap = { preset = "default" },
-
-		-- appearance = {
-		-- 	-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-		-- 	-- Adjusts spacing to ensure icons are aligned
-		-- 	nerd_font_variant = "mono",
-		-- },
-	},
-	opts_extend = { "sources.default" },
+    opts = {
+        keymap = { preset = "default" },
+    },
+    opts_extend = { "sources.default" },
 }
