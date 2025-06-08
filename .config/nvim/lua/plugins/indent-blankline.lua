46ad081ed19e0991dae1ad537fc3d0ea7f95b53b
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-06-08 15:00:53 +0200

    add indent-blankline plugin for indentation lines

---
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,12 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+        scope = {
+            show_start = false,
+            show_end = false,
+        },
+    },
+}
