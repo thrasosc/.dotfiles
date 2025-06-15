398540862c0a2d4edde63bb1b9e0325ea0d34d30
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-06-15 15:03:52 +0200

    remove broken focus function

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -7,17 +7,5 @@ return {
         vim.g.vimtex_view_method = "skim"
         vim.g.vimtex_view_skim_reading_bar = 1 -- Highlight current line
         vim.g.vimtex_view_skim_sync = 1 -- Perform forward search after compilation
-
-        -- Return focus to Neovim after inverse search on macOS
-        local function TexFocusVim()
-            vim.fn.system("open -a Ghostty")
-            vim.cmd("redraw!")
-        end
-        vim.api.nvim_create_augroup("vimtex_event_focus", { clear = true })
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "VimtexEventViewReverse",
-            callback = TexFocusVim,
-            group = "vimtex_event_focus",
-        })
     end,
 }
