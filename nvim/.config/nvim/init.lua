85a74dd62193034cb9aad63d77518f450cd34ff7
thrasosc <100732009+thrasosc@users.noreply.github.com>
2025-07-23 14:41:23 +0300

    update dotfiles

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,8 @@ vim.opt.clipboard = "unnamedplus"
 -- Keymaps
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<Esc>', ':nohlsearch<cr>')
+vim.keymap.set('n', 'c', '"_c', { noremap = true })
+vim.keymap.set('n', 'C', '"_C', { noremap = true })
 
 -- Search
 vim.opt.ignorecase = true -- Ignore case in search patterns
